1572eb9c828c7949e682e123857a78fcf80c5f03
Waseem <waseem7356@gmail.com>
2025-02-26 21:48:53 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const { process } = require('ipaddr.js')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get(`/youtube` , (req,res) =>{
+res.send("hello World");
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+console.log("gggggggg");
